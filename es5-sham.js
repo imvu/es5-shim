a62932bb6c8557c0b9092af65f2c13b1486f4355
Brian Di Palma <offler@gmail.com>
2013-11-12 09:27:51 +0000

    Added comment next to semicolon to explain their presence.

--- es5-sham.js
+++ es5-sham.js
@@ -8,8 +8,8 @@
 
 //Add semicolon to prevent IIFE from being passed as argument to concated code.
 ;
+
 // Module systems magic dance
-;
 (function (definition) {
     definition();
 })(function () {
--- es5-shim.js
+++ es5-shim.js
@@ -6,6 +6,7 @@
 
 // vim: ts=4 sts=4 sw=4 expandtab
 
+//Add semicolon to prevent IIFE from being passed as argument to concated code.
 ;
 
 // UMD (Universal Module Definition)
